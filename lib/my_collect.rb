567cf9514284806848daf20a94026aaf785c3ce1
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-06 15:16:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
-  new_array=[]
   i=0 
   while i<array.count
-  new_collection<< yield(array[i])
+  collection<< yield(array[i])
   i=i+1 
 end 
-new_collection
+collection
 end
 
 
