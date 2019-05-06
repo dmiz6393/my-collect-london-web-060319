ef9bb892d9a05f46a6e2186e497e7c76a61de866
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-06 14:55:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(collection)
+  new_collection=[]
   i=0 
   while i<collection.length
   yield collection[i].upcase
   i=i+1 
+  new_collection<< collection[i].upcase
 end 
-collection 
 end 
 
