5c7e8a112ed9f11b84e77ec6cf8af27f4e7beee8
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-06 15:05:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,3 +10,14 @@ end
 new_collection
 end 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+def my_collect(array)
+  new_array=[]
+  i=0 
+  while i<array.length
+  new_array<<yield (array.split("").first)
+  i=i+1 
+end
+new_array
+end 
