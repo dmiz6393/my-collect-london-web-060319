35d62daae9a5e366b294ae034876a62e8105a841
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-06 15:10:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,6 +8,11 @@ def my_collect(collection)
   i=i+1 
 end 
 new_collection
-end 
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
 
